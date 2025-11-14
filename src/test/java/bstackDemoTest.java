ea86248e2cd5f23fcb7529c7b265917b8fbeb663
adarshTS <adarsh.ts8@gmail.com>
2025-11-14 12:54:56 +0530

    sdk_working_1.40.0

--- src/test/java/bstackDemoTest.java
+++ src/test/java/bstackDemoTest.java
@@ -5,12 +5,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+// import com.browserstack.vanillajava.VanillaJavaTest;
+
 
 import java.time.Duration;
+import java.util.Arrays;
 
 public class bstackDemoTest {
 
     public static void main(String[] args) {
+    //    VanillaJavaTest vanillaTest = new VanillaJavaTest()
+    //            .setTestName("bstackDemoTest")                           // MANDATORY
+    //            .setScopes(Arrays.asList("bstackDemoTest"))  // MANDATORY
+    //            .setFilePath("bstackDemoTest.java")           // MANDATORY
+    //            .setTags(Arrays.asList("smoke", "critical"));           // optional
+
         WebDriver driver = null;
 
         try {
@@ -19,6 +28,7 @@ public class bstackDemoTest {
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
 
+        //    vanillaTest.start();
 
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             driver.manage().window().maximize();
@@ -50,6 +60,8 @@ public class bstackDemoTest {
 
             System.out.println("Waiting to observe result");
             Thread.sleep(3000);
+        //    vanillaTest.finishPassed();
+
 
 
         } catch (Exception e) {
@@ -58,8 +70,10 @@ public class bstackDemoTest {
         } finally {
 
             if (driver != null) {
-                System.out.println("Step 8: Closing browser");
+                System.out.println("Closing browser");
                 driver.quit();
+            //    VanillaJavaTest.shutdown();
+
             }
         }
     }
